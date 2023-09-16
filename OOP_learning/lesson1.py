a9321b370adab9735d9567f141655093b8d77b21
eddy.di <eddy.di.fint@gmail.com>
2023-09-16 19:03:02 +0600

    lesson 4

--- OOP_learning/lesson1.py
+++ OOP_learning/lesson1.py
@@ -5,6 +5,7 @@ class Car:
     tires_num = 4
     steering_wheel_num = 1
     transmission = 'manual' # or 'automatic'
+
     # Class Constructor/Initializer (Method with a special name)
     def __init__(self):
         #Object Attributes/Variables
@@ -13,7 +14,12 @@ class Car:
         self.year = 2010
         self.color = 'Green'
         self.moon_roof = True
+        self.engine_running = False
     
     # Methods
 
-    pass
+    def start_engine(self): # method that changes an objects value denoting that it is making someting
+        self.engine_running = True 
+    
+    def stop_engine(self): # method that changes an objects value denoting that it is making someting
+        self.engine_running = False
